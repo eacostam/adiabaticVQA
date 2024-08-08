15c068e7b8529f07c1469da27c0029a4981e3cd5
eacostam <34751629+eacostam@users.noreply.github.com>
2024-08-08 14:25:26 +0200

    Updated config for AT&T symbol

--- config.py
+++ config.py
@@ -13,22 +13,22 @@ LOAD_DATA_FROM_FILE = 'FILE'
 LOAD_DATA_FROM_HARDCODED = 'HARDCODED'
 
 # Configuration Parameters 
-SYMBOL = "SAN.MC"# "SAN.MC" Banco Santander, "RVPH" Reviva Pharmaceuticals, "T" AT&T
-MAX_ROWS = 250 #25 min, -1 loads the entire dataset
+SYMBOL = "T"# "SAN.MC" Banco Santander, "RVPH" Reviva Pharmaceuticals, "T" AT&T
+MAX_ROWS = 150 #250 #25 min, -1 loads the entire dataset
 DEBUG_PRINT_LEN = 10
 LOG_DIR = "./log/"
-LOG_FILE = LOG_DIR + "log{}.txt".format(time.time())
+LOG_FILE = LOG_DIR + "log{}.log".format(time.time())
 LOAD_DATA_FROM = LOAD_DATA_FROM_URL
-MAX_ITER = 100 #10 min, 100 ok, Number of training iteration
-SHOTS = 1024 #10 min, 1024 ook
+MAX_ITER = 33 #10 min, 100 ok, Number of training iteration
+SHOTS = 256 #1024 #10 min, 1024
 IN_QUBITS = 2 #Input data qubits
 CIRCUIT_SIZE = IN_QUBITS * 2
-QRB_REP = 3 # Repetitions of the Quantum Recurrent Block
+QRB_REP = 2 #3 # Repetitions of the Quantum Recurrent Block
 ANGLE_SHIFT = 0.001 #Small shift to apply on further repetitions
 VQC_RANDOM_INIT = False
-DISCRETE_EXT_PARTS = 7 #7 takes very long time and memory in simulation to finish
+DISCRETE_EXT_PARTS = 9 #7 takes very long time and memory in simulation to finish
 QISKIT_REAL = False # True to run on physical hardware, False for simulation
-ADIABATIC_REAL = False # True to run on physical hardware, False for simulation
+ADIABATIC_REAL = True #True # True to run on physical hardware, False for simulation
 
 QISKIT_HUB = 'ibm-q'
 QISKIT_GROUP = 'open'
