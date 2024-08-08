04d8032544f06b797f9f6346c76e1ea335a631dd
eacostam <34751629+eacostam@users.noreply.github.com>
2024-08-08 14:24:37 +0200

    Included timestamp to log file entries

--- helpers.py
+++ helpers.py
@@ -7,9 +7,13 @@ Created on Sun Jul 02 14:22:45 2023
 # Import Libraries
 
 import os
+import datetime
 from config import LOG_FILE
 
 # HELPER METHODS
-def qcprint(str):
+def qcprint(string):
     with open(LOG_FILE, 'a' if os.path.exists(LOG_FILE) else 'w') as f:
-        print(str, file=f)
+        now = datetime.datetime.now()
+        str2print = now.strftime("%Y-%m-%d %H:%M:%S")
+        print(str2print, file=f)
+        print(string, file=f)
